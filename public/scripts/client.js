b86fa6a7c28808291f1d6e0e6497c2e3d964dc6e
Valeria Kozlova <valeriakozlova@Valerias-MacBook-Pro.local>
2020-02-02 22:48:25 -0500

    add createTweetElement and renderTweets functions to render tweets dynamically

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,47 +1,67 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatar": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatar": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
 
 const createTweetElement = (tweetData) => {
-  const { name, avatars, handle } = tweetData.user.name;
-  const input = tweetData.content.text;
+  const { name, avatar, handle } = tweetData.user;
+  const { text } = tweetData.content;
   const dateCreated = tweetData.created_at;
-
-  const $tweet = $('<article>');
-  const $header = $('<header>', {'class': 'tweet-header'});
-  const $footer = $('<footer>', {'class': 'tweet-footer'});
-  const $text =$('<span>', {'class': 'tweet-text'});
-
-  $header.append(`
-    <div class="user-info">
-      <img class="tweet-img" src="${avatars}"/>
-      <span class="tweet-name">${name}</span>
-    </div>
-    <span class="tweet-handle">${handle}</span>
-  `);
-
-  $footer.append(formatTime(dateCreated));
-  $footer.append(`
-  <span class="tweet-icon">
-    <i class="fas fa-flag"></i>
-    <i class="fas fa-retweet"></i>
-    <i class="fas fa-heart"></i>
-  </span>`);
-
-
-  $text.text(input);
-  $tweet.append($header, $text, $footer);
-
- return $tweet;
+  return `<article class="tweet">
+            <header>
+              <div>
+                <img src=${avatar} />
+                <span>${name}</span>
+              </div>
+              <span class="tweet-handle">${handle}</span>
+            </header>
+            <section>
+              <span class="tweet-text">${text}</span>
+            </section>
+            <footer>
+              <span>${dateCreated}</span>
+              <div class="tweet-icons">
+                <i class="fas fa-flag"></i>
+                <i class="fas fa-retweet"></i>
+                <i class="fas fa-heart"></i>
+              </div>
+            </footer>
+          </article>`
  };
 
-
 const renderTweets = (tweets) => {
-  $('#tweet-container').empty();
+  $('#tweets-container').empty();
   for (const tweet of tweets) {
-    let render = createTweetElement(tweet);
-    $('#tweet-container').prepend(render);
+    const tweetElement = createTweetElement(tweet);
+    $('#tweets-container').prepend(tweetElement);
   }
-};
+}
+
+//have a look for css class conventions: https://web.compass.lighthouselabs.ca/days/w04d2/activities/324
+
+
+
+$(document).ready(() => {
+  renderTweets(data);
+})
