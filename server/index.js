7c344f736ef33821f1aebba0bd8fe1dab25ae8b6
Najib <najib.muqeem@gmail.com>
2020-02-24 19:58:21 +0000

    init commit

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
